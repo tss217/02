fe721d1c4bf2bdbfd262b886baa478527d362262
ts217 <thiago.sdaasilva22090@gmail.com>
2024-08-20 19:52:45 -0300

    02

--- registration.py
+++ registration.py
@@ -1,13 +1,17 @@
 import os
+from dateCountV2 import  DateCountVT
 #dont forget to update of path way when i  finishe
 
-class resgWoker():
+class resgWoker(DateCountVT):
     def __init__(self, name, vtDay, date, value):
+        super().__init__()
         self.ls = []
         self.name = name
         self.vtDay = vtDay
         self.date = date
         self.value = value
+        self.dataUserDic = {"name": self.name, "vtDay": self.vtDay, "date": self.date, "value": self.value}
+
     #1-check if text existe
     #2- if not it. create txt
     #3- register
@@ -20,17 +24,22 @@ class resgWoker():
         if "test02.txt" not in self.ls:
             return False
         
+        return True
+        
     def checkToMakeTxt(self):
         if not self.CheckTxtExiste():
             with open("test02.txt", "w") as file:
                 pass
     
     def registerwoker(self):
-        with open("test02.txt", "w") as file:
-            file.write(f"name: {self.name}, vtDay: {self.vtDay}, date: {self.date}, value: {self.value}"+"\n")
+        self.GetDataUser
+        self.dataUsers.append(self.dataUserDic)
+        print(self.dataUsers)
+        self.atualizar_arquivo()
+
 
 if __name__ == "__main__":
-    x = resgWoker("jaum","5","0","5.95")
+    x = resgWoker("tatat","5","0","5.95")
     print(x.CheckTxtExiste())
     x.checkToMakeTxt()
     x.registerwoker()
